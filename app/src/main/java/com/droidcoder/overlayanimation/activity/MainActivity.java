6190e7d08ebf056cf64936cee00096725c7532d2
Thanos Karpouzis <athanasios.karpouzis@goeuro.com>
2014-11-23 15:30:13 +0100

    repo init

---
+++ app/src/main/java/com/droidcoder/overlayanimation/activity/MainActivity.java
@@ -0,0 +1,63 @@
+package com.droidcoder.overlayanimation.activity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
+import android.view.inputmethod.InputMethodManager;
+import com.droidcoder.overlayanimation.R;
+import com.droidcoder.overlayanimation.animation.ResizeViewHeightAnimation;
+
+public class MainActivity extends ActionBarActivity {
+
+  private View primaryScreen;
+  private View secondaryScreen;
+  private View profileButton;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    primaryScreen = findViewById(R.id.primary_layout);
+    secondaryScreen = findViewById(R.id.secondary_layout);
+    profileButton = findViewById(R.id.fab_button);
+    profileButton.setOnClickListener(new onFabButtonClick());
+  }
+
+  private final class onFabButtonClick implements View.OnClickListener {
+
+    @Override public void onClick(View v) {
+      final int targetSize = getWindowManager().getDefaultDisplay().getHeight() / 4;
+      if (secondaryScreen.getHeight() == 0) {
+        hideKeyboard();
+        SwitchSecondaryViewVisibility(targetSize, -90, 200);
+      } else if (secondaryScreen.getHeight() == targetSize) {
+        SwitchSecondaryViewVisibility(0, 90, 200);
+      }
+    }
+  }
+
+  private void SwitchSecondaryViewVisibility(int overlaySize, int rotation, int duration) {
+    profileButton.animate()
+        .rotationBy(rotation)
+        .setDuration(duration)
+        .setInterpolator(new DecelerateInterpolator())
+        .start();
+    Animation animation = new ResizeViewHeightAnimation(secondaryScreen, overlaySize);
+    animation.setInterpolator(new DecelerateInterpolator());
+    animation.setDuration(duration);
+    secondaryScreen.startAnimation(animation);
+  }
+
+  private void hideKeyboard() {
+    View view = this.getCurrentFocus();
+    if (view != null) {
+      InputMethodManager inputManager =
+          (InputMethodManager) this.getSystemService(Context.INPUT_METHOD_SERVICE);
+      inputManager.hideSoftInputFromWindow(view.getWindowToken(),
+          InputMethodManager.HIDE_NOT_ALWAYS);
+    }
+  }
+}
---
+++ app/src/main/java/com/droidcoder/overlayanimation/animation/ResizeViewHeightAnimation.java
@@ -0,0 +1,36 @@
+package com.droidcoder.overlayanimation.animation;
+
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
+
+public class ResizeViewHeightAnimation extends Animation {
+
+  private int mHeight;
+  private int mStartHeight;
+  private View mView;
+
+  public ResizeViewHeightAnimation(View view, int width) {
+    mView = view;
+    mHeight = width;
+    mStartHeight = view.getHeight();
+  }
+
+  @Override
+  protected void applyTransformation(float interpolatedTime, Transformation t) {
+    int newWidth = mStartHeight + (int) ((mHeight - mStartHeight) * interpolatedTime);
+
+    mView.getLayoutParams().height = newWidth;
+    mView.requestLayout();
+  }
+
+  @Override
+  public void initialize(int width, int height, int parentWidth, int parentHeight) {
+    super.initialize(width, height, parentWidth, parentHeight);
+  }
+
+  @Override
+  public boolean willChangeBounds() {
+    return true;
+  }
+}
